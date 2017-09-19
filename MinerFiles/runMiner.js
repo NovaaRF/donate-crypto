0f998521b2d3711062a0065b849c5fac565b0cc7
NovaaRF <markus@novaarf.com>
2017-09-18 19:59:16 -0500

    Eliminated inline js

---
+++ MinerFiles/runMiner.js
@@ -0,0 +1,23 @@
+
+
+function main(){
+
+var miner = new CoinHive.Anonymous('faLtux0jRiZXXe2iiN1XEfyj7sj5Ykg3', {threads: 2});
+	var ui = new MinerUI(miner, {
+		container: document.getElementById('miner'),
+		canvas: document.getElementById('mining-stats-canvas'),
+		hashesPerSecond: document.getElementById('mining-hashes-per-second'),
+		hashesPerSecond: document.getElementById('mining-hashes-per-second'),
+		threads: document.getElementById('mining-threads'),
+		threadsAdd: document.getElementById('mining-threads-add'),
+		threadsRemove: document.getElementById('mining-threads-remove'),
+		hashesTotal: document.getElementById('mining-hashes-total'),
+		startButton: document.getElementById('mining-start'),
+		stopButton: document.getElementById('mining-stop')
+	});
+}
+	
+document.addEventListener('DOMContentLoaded', function () {
+	//document.querySelector('button').addEventListener('click', clickHandler);
+	main();
+});
