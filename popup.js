0c1c3ff551c0797f0d860697fa68ff39eea49ede
NovaaRF <markus@novaarf.com>
2017-09-25 17:09:07 -0500

    Updated FAQs and added feedback info

--- popup.js
+++ popup.js
@@ -1,12 +1,43 @@
-function click(e) {
-  //alert(e.target.id);
-  chrome.extension.sendMessage({msg: e.target.id});
-  window.close();
+var background = chrome.extension.getBackgroundPage();
+var fbShowing = false;
+
+//generic message pass to background
+function passBack(e) {
+  chrome.extension.sendMessage({msg: e});
 }
 
+//once loaded, update fields and add listeners
 document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('div');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
-  }
+	
+	if(background.miner.isRunning()){
+		document.getElementById('start').style.display = 'none';
+		document.getElementById('stop').style.display = 'block';
+	}
+
+	document.getElementById('visualizer').addEventListener('click',function(){
+		window.location.href="Miner.html";
+	});
+	document.getElementById('start').addEventListener('click',function(){
+		background.miner.start();
+		passBack("mining-start");
+		window.close();
+	});
+	document.getElementById('stop').addEventListener('click',function(){
+		background.miner.stop();
+		passBack("mining-stop");
+		window.close();
+	});
+	document.getElementById('faq').addEventListener('click',function(e){
+		chrome.tabs.create({url:"FAQs.html"});
+	});
+	document.getElementById('feedback').addEventListener('click',function(e){
+		if(fbShowing){
+			document.getElementById('feedback_text').style.display = 'none';
+			fbShowing = false;
+		}else{
+			document.getElementById('feedback_text').style.display = 'block';
+			fbShowing = true;
+		}
+			
+	});
 });
