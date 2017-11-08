c399271d0722719d062f5a307f3dbd93add162c8
NovaaRF <markus@novaarf.com>
2017-11-08 14:28:24 -0600

    Add files to support posting logs to AWS

---
+++ js/postItem.js
@@ -0,0 +1,34 @@
+console.log("hello there");
+
+
+AWS.config.update({
+  region: "us-east-2",
+  endpoint: 'http://localhost:8000',
+  accessKeyId: "AKIAJYURZ5OUXUO65UOQ",
+  secretAccessKey: "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"
+});
+
+
+var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+
+function postAWSlogs(dataObj){
+	
+	var params = {
+		TableName: "user_logs",
+		Item: dataObj
+	};
+	params.Item.userId = dataObj.userid +'_'+dataObj.machineID;
+	params.Item.timeStamp = Date.now();
+	
+	console.log("Adding to database:",params.Item);
+	
+	docClient.put(params, function (err, data) {
+		if (err) {
+			console.log("there was an error with PUT..");
+			console.log("Error JSON: " + JSON.stringify(err));
+		} else {
+			console.log("PUT operation succeeded");
+		}
+	});
+}
+
